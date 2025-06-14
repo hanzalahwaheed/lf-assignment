b642a4a4189e446c27bafe23e07e89ea28291191
hanzalahwaheed <hanzalah.w@gmail.com>
2025-06-15 02:26:48 +0530

    fix: switch to SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
-const Home = () => {
-  const [data, setData] = useState<string>("");
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("https://dummyjson.com/products");
-      console.log(response);
-      const data = response.data;
-      console.log(data);
-      setData(JSON.stringify(data, null, 2));
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get(
+      "https://dummyjson.com/products?limit=100"
+    );
+    if (!response.data)
+      throw new Error(`HTTP error. Status: ${response.status}`);
+    const products = response.data.products;
+    return products;
+  } catch (e) {
+    console.error(e);
+  } finally {
+  }
+};
 
   useEffect(() => {
     fetchData();
